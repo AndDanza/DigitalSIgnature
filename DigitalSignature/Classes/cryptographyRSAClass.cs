004e454001721df698772d76ff49fa7d900504d8
Andrea Danzante <anddanzan@foi.hr>
2017-10-23 19:56:35 +0200

    Kreirana klasa cryptographyRSAClass
    Klasa koja će generirati ključeve i kriptirati te dekriptirati sadržaj. Trenutno vraća string u XML formatu koji sadrži sve za javni i privatni ključ

---
+++ DigitalSignature/Classes/cryptographyRSAClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalSignature.Classes
+{
+    public class cryptographyRSAClass
+    {
+        RSACryptoServiceProvider rsaKeys;
+
+        public cryptographyRSAClass()
+        {
+            rsaKeys= new RSACryptoServiceProvider();
+        }
+
+        public string getKeys()
+        {
+            string publicPrivateXml = rsaKeys.ToXmlString(true);
+            return publicPrivateXml;
+
+        }
+    }
+}
