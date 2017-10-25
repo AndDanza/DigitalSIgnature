ea6a314aaea06b60ef4a5334bcb4b9a5f68cef02
Andrea Danzante <anddanzan@foi.hr>
2017-10-25 14:43:42 +0200

    Dodana klasa za učitani dokument

---
+++ DigitalSignature/Classes/UploadedDocument.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalSignature.Classes
+{
+    public static class UploadedDocument
+    {
+        static string fileName;
+        static string filePath;
+        static string fileContent;
+
+        public static void setFileName(string name, string path, string content)
+        {
+            fileName = name;
+            filePath = path;
+            fileContent = content;
+        }
+
+        public static string getFileName()
+        {
+            return fileName;
+        }
+
+        public static string getFilePath()
+        {
+            return filePath;
+        }
+
+        public static string getFileContent()
+        {
+            return fileContent;
+        }
+
+    }
+}
