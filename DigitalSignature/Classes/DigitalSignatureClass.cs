84752e328d86fd10db6dafb5ae5f9f98194a249a
Andrea Danzante <anddanzan@foi.hr>
2017-11-05 19:42:55 +0100

    Kreirana klasa za digitalni potpis

---
+++ DigitalSignature/Classes/DigitalSignatureClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalSignature.Classes
+{
+    class DigitalSignatureClass
+    {
+        public DigitalSignatureClass() { }
+
+        private string GetDigest(string originalText)
+        {
+            SHADigestClass shaObject = new SHADigestClass(originalText);
+            string digest = shaObject.GetDigest();
+
+            return digest;
+        }
+
+        private void SignWithRSA()
+        {
+            AsymmetricCryptography rsaObject = new AsymmetricCryptography(true);
+            rsaObject.EncryptDocumentRSA("");
+        }
+
+        public string SignDocument(string toSign)
+        {
+            string toSignDigest = GetDigest(toSign);
+
+            SignWithRSA();
+
+            return "";
+        }
+    }
+}
